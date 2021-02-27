d1abed6038270ab30ced3781638b1fe6faddd5d2
thesuhas <suhastvs@gmail.com>
2021-02-27 14:05:19 +0530

    Layout done

--- landingscreen.py
+++ landingscreen.py
@@ -16,20 +16,23 @@ from kivy.uix.boxlayout import BoxLayout
 class Test(BoxLayout):
 	def __init__(self, **kwargs):
 		super(Test, self).__init__(**kwargs)
-		self.cols = 1
-		self.img1 = Image()
+		self.orientation = 'vertical'
+		self.img1 = Image(pos_hint={"top": 1}, size_hint=(1, 0.9))
+		self.add_widget(self.img1)
+		self.output = Label(text="test", pos_hint={'bottom': 1}, size_hint=(1, 0.1))
+		self.add_widget(self.output)
 
 		# Inside
-		self.inside = BoxLayout()
-		self.inside.cols = 1
+		self.inside = FloatLayout(size_hint=(1, 0.1))
+		self.inside.cols = 2
 		self.capture = cv2.VideoCapture(0)
-		self.play = Button(text="Play")
+		self.play = Button(text="Play", size_hint=(0.5, 1), pos_hint={"left": 1})
+		self.translate = Button(text="Translate", size_hint=(0.5, 1), pos_hint={"right": 1})
 		self.inside.add_widget(self.play)
+		self.inside.add_widget(self.translate)
 		self.add_widget(self.inside)
 		#cv2.namedWindow("CV2 Image")
 		Clock.schedule_interval(self.update, 1.0/33.0)
-
-		self.add_widget(self.img1)
 
 	def update(self, dt):
 		ret, frame = self.capture.read()
