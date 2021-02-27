920a8fa0a3856ad23e8702bddedc21bb2bb5ddb6
thesuhas <suhastvs@gmail.com>
2021-02-27 20:30:13 +0530

    going back to old

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, jsonify, request, Response
-import numpy as np
-import cv2
-import jsonpickle
 
 app = Flask(__name__)
 
@@ -13,14 +10,7 @@ def nothing():
 def test():
     return jsonify({'test': 'test'})
 
-@app.route('/test', methods=['POST'])
+'''@app.route('/test', methods=['POST'])
 def fail():
-    r = request
-    nparr = np.fromstring(r.data, np.uint8)
-
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
-
-    return Response(response=response_pickled, status=200, mimetype="application/json")
+    data = request.files['data']
+    print(data)'''
